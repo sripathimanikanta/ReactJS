1a7f7ba96237f1757661140d2923dacf43685465
Manikanta <msripathi035@gmail.com>
2022-10-13 10:53:24 +0530

    trying for first deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,33 @@ import React from 'react';
 import './App.css';
 // import Anime from './Anime';
 // import Form from './Form';
-import Todolist from './Todolist';
-
+// import Todolist from './Todolist';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import store from "./store";
+import { Provider } from "react-redux";
 
 
 function App() {
   return (
     // <Anime />
     // <Form />
-    <Todolist />
+    // <Todolist />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Provider store={store}>
+            <Home />
+          </Provider>
+        }
+      />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={ <div>Error</div>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from './App';
+import App from './App';
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import store from "./store";
-import { Provider } from "react-redux";
-import Cards_Big from "./Cards_Big"
-// import testing_router from
-import Testing_Hooks from "./Testing_Hooks";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Provider store={store}>
-              <Home />
-            </Provider>
-          }
-        />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-    {/* <Cards_Big />
-    {/* <Testing_Hooks /> */}
+    <App />
   </React.StrictMode>
 );
 
